91d9c90d0f6bea9111443784d753f79b31ce611f
LucasSachs <109156708+LucasSachs@users.noreply.github.com>
2023-05-08 15:16:35 -0300

    Primeiro commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    public function index() {
+        $nome = 'Felipe Santos';
+
+        return view('hello', compact('nome'));
+    }
+
+    use AuthorizesRequests, ValidatesRequests;
+}
